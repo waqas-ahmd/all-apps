fa685734f572e90c1fc348645e7958ffadf9c645
waqas-ahmd <65waqas@gmail.com>
2020-12-13 16:04:06 +0500

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import links from "./links";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {links.map((link) => (
+        <div key={link.Id}>
+          <a href={link.URL}>{link.Title}</a>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ src/links.js
@@ -0,0 +1,39 @@
+const links = [
+  {
+    Id: 1,
+    Title: "Expense Tracker App",
+    URL: "https://hardcore-kowalevski-533414.netlify.app/",
+  },
+  {
+    Id: 2,
+    Title: "Covid Tracker App",
+    URL: "https://ecstatic-stonebraker-65a7bc.netlify.app/",
+  },
+  {
+    Id: 3,
+    Title: "Shoe Store App",
+    URL: "https://unruffled-wozniak-700f77.netlify.app/",
+  },
+  {
+    Id: 4,
+    Title: "Web Animation App",
+    URL: "https://fervent-stonebraker-bd5bf8.netlify.app/",
+  },
+  {
+    Id: 5,
+    Title: "Web Animation App",
+    URL: "https://happy-albattani-62b3c5.netlify.app/",
+  },
+  {
+    Id: 6,
+    Title: "Quiz App",
+    URL: "https://goofy-stonebraker-e9bbaf.netlify.app/",
+  },
+  {
+    Id: 7,
+    Title: "Timer App",
+    URL: "https://hopeful-carson-391c27.netlify.app/",
+  },
+];
+
+export default links;
